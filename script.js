c2837fce632c3a3e5b6d440189a276552b6719ac
ArminderK <arminder.khinda@yahoo.com>
2017-09-29 10:11:05 -0700

    Added javascript and html code

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 $(function() {
   
-  var words = ["adventure", "hello"];
+  var words = ["ADVENTURE", "HELLO", "GOOD", "SUCK"];
+  var word = words[Math.floor(Math.random() * words.length)];
+  var counter = 0;
+  console.log(word);
   
   $('button').click(function() {
     var input = $(this).text();
-    
+    for(var i = 0; i<word.length; i++) {
+      if(input === word.charAt(i)) {
+        console.log(input);
+      }
+      else{
+        counter++;
+        $('#misses').html('Misses: ' + counter + "/" + word.length);
+      }
+    }
   });
-  
 });
