9b8d95deef2aab0e70945ff06f677f3c5d8e3458
arminderk <arminder.khinda@yahoo.com>
2017-09-29 12:59:26 -0700

    Added code to display word on the page

--- script.js
+++ script.js
@@ -2,19 +2,35 @@ $(function() {
   
   var words = ["ADVENTURE", "HELLO", "GOOD", "SUCK"];
   var word = words[Math.floor(Math.random() * words.length)];
-  var counter = 0;
-  console.log(word);
+ 
+  var letter = '';
+  var charWords = '';
   
-  $('button').click(function() {
-    var input = $(this).text();
-    for(var i = 0; i<word.length; i++) {
-      if(input === word.charAt(i)) {
-        console.log(input);
-      }
-      else{
-        counter++;
-        $('#misses').html('Misses: ' + counter + "/" + word.length);
-      }
-    }
-  });
+  console.log(word);
+      
+      charWords = word.split('');
+      
+      $('#chars').text(charWords).css("visibility", "hidden");
+                             
+      $('button').click(function() {
+         var counter = 0;
+        for(var i = 0; i<word.length; i++) {
+          letter = word.charAt(i);
+          var input = $(this).text();
+          if(input === letter) {
+            console.log(input);
+          }
+          else{
+            counter++;
+            $('#misses').html('Misses: ' + counter + "/" + word.length);
+          }
+        }
+        
+        if(counter == word.length) {
+          console.log("You Lose!");
+          $('button').unbind('click');
+        }
+        
+      });
+ 
 });
