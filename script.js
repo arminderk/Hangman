b03525b835ea495c415c38a9be73c63e663a17b8
ArminderK <arminder.khinda@yahoo.com>
2017-09-29 16:24:00 -0700

    Counter works now. Removed the for loop.

--- script.js
+++ script.js
@@ -2,35 +2,32 @@ $(function() {
   
   var words = ["ADVENTURE", "HELLO", "GOOD", "SUCK"];
   var word = words[Math.floor(Math.random() * words.length)];
- 
+  var counter = 0;
   var letter = '';
   var charWords = '';
   
   console.log(word);
-      
-      charWords = word.split('');
-      
-      $('#chars').text(charWords).css("visibility", "hidden");
-                             
-      $('button').click(function() {
-         var counter = 0;
-        for(var i = 0; i<word.length; i++) {
-          letter = word.charAt(i);
-          var input = $(this).text();
-          if(input === letter) {
-            console.log(input);
-          }
-          else{
-            counter++;
-            $('#misses').html('Misses: ' + counter + "/" + word.length);
-          }
-        }
-        
-        if(counter == word.length) {
-          console.log("You Lose!");
-          $('button').unbind('click');
-        }
-        
-      });
+  
+//  charWords = word.split('');
+//
+//  $('#chars').text(charWords).css("visibility", "hidden");
+
+  $('button').click(function() {
+    var input = $(this).text();
+    
+      if(word.includes(input)) {
+        console.log(input);
+      }
+      else{
+        counter += 1;
+        $('#misses').html('Misses: ' + counter + "/" + word.length);
+      }
+
+    if(counter == word.length) {
+      console.log("You Lose!");
+      $('button').unbind('click');
+    }
+    
+  });
  
 });
